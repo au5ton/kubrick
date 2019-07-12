14576e46d4b9315e930cd02a71eb75eeb886a938
Austin Jackson <themacphage@gmail.com>
2019-07-12 16:01:07 -0500

    new seeking method

--- lib/common.sh
+++ lib/common.sh
@@ -23,3 +23,38 @@ fontcheck() {
         return 0
     fi
 }
+
+# https://unix.stackexchange.com/a/426827
+# converts HH:MM:SS.sss to fractional seconds
+codes2seconds() (
+    local hh=${1%%:*}
+    local rest=${1#*:}
+    local mm=${rest%%:*}
+    local ss=${rest#*:}
+    printf "%s" $(bc <<< "$hh * 60 * 60 + $mm * 60 + $ss")
+)
+
+# converts fractional seconds to HH:MM:SS.sss
+seconds2codes() (
+    local seconds=$1
+    local hh=$(bc <<< "scale=0; $seconds / 3600")
+    local remainder=$(bc <<< "$seconds % 3600")
+    local mm=$(bc <<< "scale=0; $remainder / 60")
+    local ss=$(bc <<< "$remainder % 60")
+    printf "%02d:%02d:%06.3f" "$hh" "$mm" "$ss"
+)
+
+subtracttimes() (
+    local t1sec=$(codes2seconds "$1")
+    local t2sec=$(codes2seconds "$2")
+    printf "%s" $(bc <<< "$t2sec - $t1sec")
+)
+
+# https://stackoverflow.com/a/17695543/4852536
+function ask_yes_or_no() {
+    read -p "$1 ([y]es or [N]o): "
+    case $(echo $REPLY | tr '[A-Z]' '[a-z]') in
+        y|yes) echo "yes" ;;
+        *)     echo "no" ;;
+    esac
+}
--- tool/trim.sh
+++ tool/trim.sh
@@ -3,7 +3,21 @@ DIR=`cd $(dirname "$0"); cd ..; pwd` && . $DIR/lib/common.sh
 if ishelp $1; then
     echo "$0 <input.ext> <output.ext> <start> <end>"
     echo -e "\tTime unit syntax: HOURS:MM:SS.MILLISECONDS"
+    echo -e "\tMust specify all units, no abbreviations."
     exit
 fi
 
-time ffmpeg -i "$1" -ss "$3" -to "$4" -c copy -map 0 "$2"
+# start timecode in seconds
+start=$(codes2seconds "$3")
+# one second before the desired start time
+preseek=$(bc <<< "$start - 1")
+# duration of the clip
+duration=$(subtracttimes "$3" "$4")
+# convert seconds to code for -ss
+preseek=$(seconds2codes "$preseek")
+
+# old way
+# time ffmpeg -i "$1" -ss "$3" -to "$4" -c copy -map 0 "$2"
+
+# new way
+time ffmpeg -ss "$preseek" -i "$1" -ss 00:01:00 -t "$duration" -c copy -map 0 "$2"
