bbd5f0d06b3faa988e152b3d09b263f7a74095e6
Austin Jackson <themacphage@gmail.com>
2020-02-24 01:39:32 -0600

    libx265 experiment

---
+++ experiment/hevc/README.md
@@ -0,0 +1,11 @@
+# HEVC experiment
+
+## Dependencies
+- `pip3 install -r requirements.txt`
+- `optipng` in PATH, see: http://optipng.sourceforge.net/
+    - Linux Mint repo: https://community.linuxmint.com/software/view/optipng
+    - Debian repo: https://packages.debian.org/buster/optipng
+
+## Usage
+
+`./experiment.py --help`
--- experiment/hevc/experiment.py
+++ experiment/hevc/experiment.py
@@ -11,19 +11,23 @@ from time import time
 from tqdm import tqdm
 from halo import Halo
 import GPUtil
+import cpuinfo
 
 parser = argparse.ArgumentParser(description='experiment')
 parser.add_argument('samples', type=str, nargs='+',
                     help='A set of sample clips that ffmpeg can transcode')
-parser.add_argument('--report', dest='report_dir', type=str, required=True,
+parser.add_argument('--report-directory', dest='report_dir', type=str, required=True,
                     help='Location to output report data')
-parser.add_argument('--screenfreq', dest='screenshot_frequency', type=int, default=4,
-                    help='How many seconds between screenshots in the sample files (default: 4 seconds)')
+parser.add_argument('--screenshot-frequency', dest='screenshot_frequency', type=int, default=10,
+                    help='How many seconds between screenshots in the sample files (default: 10)')
+parser.add_argument('--compress-screenshots', dest='screenshot_compression', action='store_true',
+                    help='When set, optipng will be run with argument `-o2` for 38 percent smaller PNG files with no data loss')
 args = parser.parse_args()
 
 '''
 tests:
 - libx265, crf 24-28, presets ['ultrafast', 'superfast', 'veryfast', 'faster', 'fast', 'medium', 'slow', 'slower', 'veryslow']
+    ffmpeg -i clip_1.mkv -c:v libx265 -crf 24 -preset slow -c:a copy out.mkv
 - hevc_nvenc, 
     -cq:v [0,51]
     ffmpeg -i clip_1.mkv -c:v hevc_nvenc -rc:v vbr_hq -cq:v 0 -b:v 100000k -maxrate:v 100000k -c:a copy out.mkv
@@ -70,8 +74,32 @@ def folder_scaffold(report_dir):
             print(f'couldnt create directory: {f}')
             exit(1)
 
-#def experiment_libx265(source_file, crf, preset):
-    #
+def experiment_libx265(source_file, crf, preset, writer):
+    # ffmpeg -i clip_1.mkv -c:v libx265 -crf 24 -preset slow -c:a copy out.mkv
+    src_name, src_ext = os.path.splitext(source_file)
+    src_bn = os.path.basename(src_name)
+
+    temp_name = os.path.join(report_dir, 'libx265', f'{src_bn}___temp___{src_ext}')
+    # %06d
+    screenshots_format = f'{src_bn}___%05dpos_{preset}_crf{crf}.png' # clip_1___00012pos_superfast_crf22.png 
+
+    start_time = time()
+    cmd = f'ffpb -i {source_file} -c:v libx265 -crf {crf} -preset {preset} -c:a copy {temp_name}'
+    subprocess.run(cmd, shell=True)
+    sys.stdout.write("\033[F") #back to previous line
+    sys.stdout.write("\033[K") #clear line
+    elapsed_time = time() - start_time
+    output_size = os.path.getsize(temp_name)
+    original_size = os.path.getsize(source_file)
+    # source_file, encoder, cpu_info, gpu_info, crf_value, preset, cq_value, encode_time, encode_size, original_size, command_used
+    writer.writerow([source_file, 'libx265', cpu_brand, gpu_name, crf, preset, None, elapsed_time, output_size, original_size, cmd])
+    # write screenshots for every 4 seconds of video
+    # ffmpeg -i clip_1.mkv -vf fps=0.5 "output%06d.png"
+    subprocess.run(f'ffpb -i {temp_name} -vf fps={1/args.screenshot_frequency} {os.path.join(report_dir, "libx265", screenshots_format)}', shell=True)
+    os.remove(temp_name)
+    sys.stdout.write("\033[F") #back to previous line
+    sys.stdout.write("\033[K") #clear line
+    optimize_pngs(os.path.join(report_dir, 'libx265'), f'{src_bn}*_{preset}_crf{crf}.png')
 
 def experiment_hevc_nvenc(source_file, cq, writer):
     # ffmpeg -i clip_1.mkv -c:v hevc_nvenc -rc:v vbr_hq -cq:v 0 -b:v 100000k -maxrate:v 100000k -c:a copy out.mkv
@@ -80,25 +108,39 @@ def experiment_hevc_nvenc(source_file, cq, writer):
 
     temp_name = os.path.join(report_dir, 'hevc_nvenc', f'{src_bn}___temp___{src_ext}')
     # %06d
-    screenshots_format = f'{src_bn}___%05dpos_cq{cq}.webp' # clip_1___00012pos_cq0.png 
+    screenshots_format = f'{src_bn}___%05dpos_cq{cq}.png'
 
     start_time = time()
-    subprocess.run(f'ffpb -i {source_file} -c:v hevc_nvenc -rc:v vbr_hq -cq:v {cq} -b:v 100000k -maxrate:v 100000k -c:a copy {temp_name}', shell=True)
+    cmd = f'ffpb -i {source_file} -c:v hevc_nvenc -rc:v vbr_hq -cq:v {cq} -b:v 100000k -maxrate:v 100000k -c:a copy {temp_name}'
+    subprocess.run(cmd, shell=True)
     sys.stdout.write("\033[F") #back to previous line
     sys.stdout.write("\033[K") #clear line
     elapsed_time = time() - start_time
     output_size = os.path.getsize(temp_name)
-    # source_file, encoder, gpu_info, crf_value, preset, cq_value, encode_time, encode_size
-    writer.writerow([src_bn, 'hevc_nvenc', gpu_name, None, None, cq, elapsed_time, output_size])
+    original_size = os.path.getsize(source_file)
+    # source_file, encoder, gpu_info, crf_value, preset, cq_value, encode_time, encode_size, original_size, command_used
+    writer.writerow([source_file, 'hevc_nvenc', None, gpu_name, None, None, cq, elapsed_time, output_size, original_size, cmd])
     # write screenshots for every 4 seconds of video
     # ffmpeg -i clip_1.mkv -vf fps=0.5 "output%06d.png"
     subprocess.run(f'ffpb -i {temp_name} -vf fps={1/args.screenshot_frequency} {os.path.join(report_dir, "hevc_nvenc", screenshots_format)}', shell=True)
+    os.remove(temp_name)
     sys.stdout.write("\033[F") #back to previous line
     sys.stdout.write("\033[K") #clear line
-    os.remove(temp_name)
+    #subprocess.run(f'find {os.path.join(report_dir, "hevc_nvenc")} -name "{src_bn}*_cq{cq}.png" -exec optipng -silent {{}} \\;', shell=True)
+    optimize_pngs(os.path.join(report_dir, 'hevc_nvenc'), f'{src_bn}*_cq{cq}.png')
+
+def optimize_pngs(path, mask):
+    output = subprocess.check_output(['find', path, '-name', mask])
+    pngs = output.decode('utf-8').split('\n')
+    pngs = sorted(pngs)
+    for pic in tqdm(pngs, desc='Optimizing screenshot PNGs on disk (lossless)', unit='file', position=0):
+        subprocess.run(['optipng', ('-o2' if args.screenshot_compression else '-o0'), '-silent', pic])
+    sys.stdout.write("\033[F") #back to previous line
+    sys.stdout.write("\033[K") #clear line
 
 # get GPU info
 gpu_name = GPUtil.getGPUs()[0].name if (len(GPUtil.getGPUs()) > 0) else 'indeterminate'
+cpu_brand = cpuinfo.get_cpu_info()['brand']
 
 # convert to absolute path
 samples = [os.path.abspath(item) for item in args.samples]
@@ -109,7 +151,7 @@ folder_scaffold(os.path.abspath(args.report_dir))
 
 # libx265
 libx265 = dict()
-libx265['crf_range'] = range(24,28)
+libx265['crf_range'] = range(27,28)
 libx265['presets'] = ['ultrafast', 'superfast', 'veryfast', 'faster', 'fast', 'medium', 'slow', 'slower', 'veryslow']
 libx265['dir'] = os.path.join(report_dir, 'libx265')
 
@@ -119,13 +161,22 @@ hevc_nvenc['cq_range'] = range(0,51)
 hevc_nvenc['dir'] = os.path.join(report_dir, 'hevc_nvenc')
 
 # for every sample
-for src in tqdm(samples, desc='Source sample files', unit='files', position=2):
+for src in tqdm(samples, desc='Source sample files', unit='file', position=3):
     with open(os.path.join(report_dir, f'{os.path.basename(src)}.csv'), 'w') as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(['source_file', 'encoder', 'gpu_info', 'crf_value', 'preset', 'cq_value', 'encode_time', 'encode_size'])
+        writer.writerow(['source_file', 'encoder', 'cpu_info', 'gpu_info', 'crf_value', 'preset', 'cq_value', 'encode_time', 'encode_size', 'original_size', 'command_used'])
         
         # GPU experiment first
-        for cq in tqdm(hevc_nvenc['cq_range'], desc='Testing hevc_nvenc -cq <float> (constant quality factor)', unit='experiments', position=1):
+        for cq in tqdm(hevc_nvenc['cq_range'], desc='Testing hevc_nvenc -cq <int> (constant quality factor)', unit='experiment', position=2):
+            continue
             experiment_hevc_nvenc(src, cq, writer)
+        sys.stdout.write("\033[F") #back to previous line
+        sys.stdout.write("\033[K") #clear line
+        # CPU experiment next
+        for preset in tqdm(libx265['presets'], desc='Testing libx265 -preset <name> (fast, slow, etc)', unit='preset', position=2):
+            for crf in tqdm(libx265['crf_range'], desc='Testing libx265 -crf <int> (constant rate factor)', unit='experiment', position=1):
+                experiment_libx265(src, crf, preset, writer)
+            sys.stdout.write("\033[F") #back to previous line
+            sys.stdout.write("\033[K") #clear line
 
 exit()
